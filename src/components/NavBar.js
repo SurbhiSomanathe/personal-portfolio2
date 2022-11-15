74bdd088c43c67e5aab58397744cf3decd4965ca
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-11-15 18:55:53 +0530

    afer cv

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,8 +50,8 @@ export const NavBar = () => {
             </Navbar.Collapse>
           </Container>
           {
-            <a
-                href="https://docs.google.com/document/d/12COICQfw93xeLRnNL7L3RWcvCn7iOaQ4/edit?usp=sharing&ouid=102339501710590195652&rtpof=true&sd=true"
+            <a 
+                href="https://drive.google.com/file/d/1iD7HyrmccKZiT560bIHAmCPRs6GMaFHE/view?usp=share_link"
                 target={"_blank"}
                 className="background-color: transparent" rel="noreferrer">
                 <AiOutlineArrowDown className="text-white transform transition-transform ml-2" />
