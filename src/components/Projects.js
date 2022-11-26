91d23e2f57b3337b37e799d0f5edb554709edb9c
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-11-26 17:37:00 +0530

    final with all changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -101,13 +101,15 @@ return (
     <div className="sur">
       <div className="card-body">
         <h3 className="card-title">BlogApp</h3>
-        <a href="https://fluffy-zuccutto-a85b65.netlify.app/" target = {"_blank"} rel="noreferrer">
+        <a href="https://transcendent-florentine-7b5bd9.netlify.app/" target = {"_blank"} rel="noreferrer">
             <img src={blogApp} alt="" />
           </a>
-        <p className="card-text">In these app you write blogs which you like too. With Adding pictures too. And once you move on all blogs, blogs are available inside the database of the multiple users same as Instagram or the Facebook as well. </p>
+        <p className="card-text">In these app you write blogs which you like too. With Adding pictures too. 
+        And once you move on all blogs, blogs are available inside the database of the multiple users 
+        same as Instagram or the Facebook as well.</p>
         <p className="card-text1"> html, css, bootstrap, react, redux, router, nodejs, express, mongodb</p>
         <a href="https://github.com/SurbhiSomanathe/New_Blog_App" className="ranu" target = {"_blank"} rel="noreferrer"><Github color="black" size="30px" /></a>
-        <a href="https://fluffy-zuccutto-a85b65.netlify.app/" className="ranu" target = {"_blank"} rel="noreferrer"> <SiNetlify color="skyblue" size="30px" /></a>
+        <a href="https://transcendent-florentine-7b5bd9.netlify.app/" className="ranu" target = {"_blank"} rel="noreferrer"> <SiNetlify color="skyblue" size="30px" /></a>
       </div>
     </div>
   </div>
